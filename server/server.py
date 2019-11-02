65dc1be076a1a9443b6914692eaf0a8b8abe370d
BlackBracken <me@bracken.black>
2019-11-03 07:25:24 +0900

    return plain json contains minimal effects

--- server/server.py
+++ server/server.py
@@ -1,10 +1,28 @@
+from flask import Flask
+import json
 import serial
 import sys
 import threading
-import time
 
 serial_port = sys.argv[1]
 ser = serial.Serial(serial_port, 9600, timeout=None)
+
+app = Flask(__name__)
+
+temperature = 20.0
+humidity = 50.0
+
+class HealthEffect:
+    def __init__(self):
+        self.temperature = temperature
+        self.humidity = humidity
+
+    def jsonize(self) -> str:
+        return json.dumps(self.__dict__)
+
+@app.route('/')
+def index() -> str:
+    return HealthEffect().jsonize() 
 
 def fetch_from_serial():
     while True:
@@ -13,6 +31,8 @@ def fetch_from_serial():
 
 def main():
     threading.Thread(target=fetch_from_serial).start()
+    app.debug = True
+    app.run(host='0.0.0.0')
 
 if __name__ == '__main__':
     main()
