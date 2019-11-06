162eac2e80f7e014b52eb75c8cf4f8f9272a2aa2
BlackBracken <me@bracken.black>
2019-11-07 04:59:05 +0900

    change color by effects

--- client/HealthAlert-desktop/Assets/Scripts/Clock/HealthEffectRetriever.cs
+++ client/HealthAlert-desktop/Assets/Scripts/Clock/HealthEffectRetriever.cs
@@ -9,6 +9,7 @@ public class HealthEffectRetriever : MonoBehaviour
     public Text humidityText;
     public Text climateText;
     public Text dryText;
+    public ParticleSystem ringParticpe;
 
     private float _currentTime;
 
@@ -46,26 +47,31 @@ public class HealthEffectRetriever : MonoBehaviour
         temperatureText.text = string.Format("{0}°C", healthEffectData.temperature);
         humidityText.text = string.Format("{0}%", healthEffectData.humidity);
         
-        switch (healthEffectData.climate_effect)
+        switch (healthEffectData.climate_effect.ToUpper())
         {
             case "HEATSTROKE_NOTICE":
                 climateText.text = "熱中症に注意";
+                ringParticpe.startColor = Color.yellow;
                 break;
             case "HEATSTROKE_ALERT":
                 climateText.text = "熱中症に警戒を!";
+                ringParticpe.startColor = Color.red;
                 break;
             case "VIRUS_NOTICE":
                 climateText.text = "インフルエンザに注意";
+                ringParticpe.startColor = Color.cyan;
                 break;
             case "VIRUS_ALERT":
                 climateText.text = "インフルエンザに警戒を!";
+                ringParticpe.startColor = Color.magenta;
                 break;
             default:
                 climateText.text = "今日も良い1日を";
+                ringParticpe.startColor = Color.green;
                 break;
         }
 
-        switch (healthEffectData.dry_effect)
+        switch (healthEffectData.dry_effect.ToUpper())
         {
             case "DRY":
                 dryText.text = "乾燥情報: 乾き気味";
